33621e0dc27b43cf5762c6c8f5ca429fddce444a
Aar Kaiser <dev@aarkaiser>
2021-11-10 20:35:23 -0500

    updating complete Productjs

--- models/Product.js
+++ models/Product.js
@@ -8,6 +8,7 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+
 Product.init(
   { id: {
     type: DataTypes.INTEGER,
@@ -15,6 +16,32 @@ Product.init(
     primaryKey: true,
     autoIncrement: true,
   },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model:'category',
+      key:'id',
+    },
+  }
     // define columns
   },
   {
