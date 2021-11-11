d99814d2eb470ac354f3233947ea2489e7f2067b
Aar Kaiser <dev@aarkaiser>
2021-11-10 20:19:45 -0500

    updating categoryjs

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
-  {
+  { id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // define columns
   },
   {
