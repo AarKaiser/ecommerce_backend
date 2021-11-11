b2ed611f184b22510da635af9aab5258865adf8c
Aar Kaiser <dev@aarkaiser>
2021-11-10 20:22:22 -0500

    updating Productjs

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -8,7 +9,12 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
+  { id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
     // define columns
   },
   {
