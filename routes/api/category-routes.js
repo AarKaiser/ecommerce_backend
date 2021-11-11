688de6c97d5adace810836e5b8b92f0572de87b0
Aar Kaiser <dev@aarkaiser>
2021-11-10 22:13:32 -0500

    updating completed category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,6 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-
   // find all categories
   // be sure to include its associated Products
 
@@ -20,7 +19,6 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-
   // find one category by its `id` value
   // be sure to include its associated Products
 
@@ -35,19 +33,57 @@ router.get("/:id", (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  console.log(icatData);
+  // console.log(icatData);
 });
 
 router.post("/", (req, res) => {
   // create a new category
+
+  try {
+    const ncatData = await Category.create(req.body);
+    res.status(200).json(ncatData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+
+  try {
+    const ccatData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!ccatData[0]) {
+      res.status(404).json({ message: "Category ID does not exist!" });
+    }
+    res.status(200).json(`Category changed to ${req.body.category_name}`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+
+  try {
+    const dcatData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(dcatData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
