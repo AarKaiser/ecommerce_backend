41e1de255622eb9cb941ed77dbd854820defb8ca
Aar Kaiser <dev@aarkaiser>
2021-11-10 23:46:40 -0500

    updating changes

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.listen(PORT, () => {
 });
 
 
-// Fill Out the API Routes to Perform RESTful CRUD Operations
-// Fill out the unfinished routes in product-routes.js, tag-routes.js, and category-routes.js to perform create, read, update, and delete operations using your Sequelize models.
-// Note that the functionality for creating the many-to-many relationship for products has already been completed for you.
+// Fill Out the API Routes to Perform RESTful CRUD Operations √
+// Fill out the unfinished routes in product-routes.js, tag-routes.js, and category-routes.js to perform create, read, update, and delete operations using your Sequelize models. √
+// Note that the functionality for creating the many-to-many relationship for products has already been completed for you. √ 🎉
 
-// Hint: Be sure to look at the mini-project code for syntax help and use your model's column definitions to figure out what req.body will be for POST and PUT routes!
+// Hint: Be sure to look at the mini-project code for syntax help and use your model's column definitions to figure out what req.body will be for POST and PUT routes! √√√ would've been impossible without the mini-project! lol.
 
 
 // Seed the Database
-// After creating the models and routes, run npm run seed to seed data to your database so that you can test your routes.
+// After creating the models and routes, run npm run seed to seed data to your database so that you can test your routes. √
 
-// Sync Sequelize to the Database on Server Start
-// Create the code needed in server.js to sync the Sequelize models to the MySQL database on server start.
+// Sync Sequelize to the Database on Server Start √
+// Create the code needed in server.js to sync the Sequelize models to the MySQL database on server start. √
